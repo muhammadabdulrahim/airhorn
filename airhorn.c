17012db2dc81071179b9a2939d11c30086212def
Muhammad Abdul-Rahim <moobeidallah@gmail.com>
2017-04-05 23:47:57 -0700

    Working initial draft

--- airhorn.c
+++ airhorn.c
@@ -11,6 +11,12 @@
 const UINT Z_KEY = 0x5A;
 const char g_class_name[] = "Airhorn";
 
+/* FMOD globals */
+FMOD_RESULT		result;
+FMOD_SYSTEM		*fmod_system;
+FMOD_SOUND		*sound;
+FMOD_CHANNEL		*channel = 0;
+FMOD_CHANNELGROUP	*channelgroup = 0;
 
 /* Handle callbacks */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -18,7 +24,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	switch(msg)
 	{
 		case WM_HOTKEY:
-			//PlaySound(TEXT("recycle.wav"),NULL,SND_FILENAME);
+			result = FMOD_System_PlaySound(fmod_system,sound,channelgroup,0,&channel);
 			return 0;
 		case WM_CLOSE:
 			DestroyWindow(hwnd);
@@ -76,6 +82,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 
 	// Sample the audio into memory
+	result = FMOD_System_Create(&fmod_system);
+	result = FMOD_System_Init(fmod_system,32,FMOD_INIT_NORMAL,0);
+	result = FMOD_System_CreateSound(fmod_system,"airhorn.wav",FMOD_DEFAULT,0,&sound);
+	
+
+	if( result!=FMOD_OK )
+	{
+		MessageBox(NULL,"Error","Error",MB_ICONEXCLAMATION|MB_OK);
+		return 0;
+	}
 
 	// Show the window
 	ShowWindow(hwnd,nCmdShow);
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CC	:= /usr/bin/x86_64-w64-mingw32-gcc
-CFLAGS	:= -g -Wall -Wl,--subsystem,windows
+CFLAGS	:= -g -Wall -Wl,--subsystem,windows -Llib -lfmod64
 SRCS	:= airhorn.c
 OBJS	:= ${SRCS:.c=.o}
 TARGET	:= ${SRCS:.c=.exe}
